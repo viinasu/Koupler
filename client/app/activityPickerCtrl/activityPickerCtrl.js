cfcbf9cc3ac0e60a06898f672d5f8a2c847a06fa
Jackson Sierra <jks718@gmail.com>
2015-09-07 14:46:00 -0700

    [refactor] (client): extend activity picker controller to activities factory (by Shreeya)

--- client/app/activityPickerCtrl/activityPickerCtrl.js
+++ client/app/activityPickerCtrl/activityPickerCtrl.js
@@ -1,8 +1,6 @@
 angular.module('koupler.activities', [])
 
-.controller('activityPickerCtrl', function($scope, Activities) {
-  //need to listen for an activity getting chosen
-  var chosenActivity = //to be completed
-  //need to call getCouples from own factory
-  $scope.getCouples(chosenActivity);
+.controller('ActivityPickerCtrl', function($scope, Activities) {
+  //extend the scope with the factory
+  angular.extend($scope, Activities);
 })
