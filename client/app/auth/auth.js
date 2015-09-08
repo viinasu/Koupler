91263294d51c5b7f69ace2df535d99c3f695a45a
Jackson Sierra <jks718@gmail.com>
2015-09-08 03:20:16 -0700

    [fix] (client): align post request urls with server routing

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,68 +2,72 @@
 //signup/signin form using the injected AuthTokenFactory service. 
 angular.module('koupler.auth', [])
 
-.controller('AuthCtrl', function ($scope, $window, $location, AuthTokenFactory){
+.controller('AuthCtrl', function ($scope, $http, $window, $location, AuthTokenFactory){
       
-      var user = {};
-      //To Do add post request handlers to factories.js 
-      $scope.signin = function(){
-        $http.post('/signin', {username: $scope.usernameSignIn, password: $scope.passwordSignIn})
-            .then(function (response){
-            AuthTokenFactory.setToken(response.data.token);
-            $location.path('/activities');
-        }, 
-        function(err){
-          console.log(err);
-        })
-      }
+  var user = {};
+  //To Do add post request handlers to factories.js 
+  $scope.signin = function() {
+    $http.post('/couples/signin', {
+        username: $scope.usernameSignIn, 
+        password: $scope.passwordSignIn
+      })
+      .then(function (response) {
+        AuthTokenFactory.setToken(response.data.token);
+        $location.path('/activities');
+      }, 
+      function(err){
+        console.log(err);
+      });
+  };
 
-      $scope.signup = function(){
-        $http.post('/signup', {
-            username: $scope.usernameSignup, 
-            password: $scope.passwordSignup,
-            firstName1: $scope.firstName1Signup,
-            lastName1: $scope.lastName1Signup,
-            firstName2: $scope.firstName2Signup,
-            lastName2: $scope.lastName2Signup,
-            email: $scope.emailSignup,
-            phoneNumber: $scope.phoneNumberSignup})
-        .then(function(response){
-          AuthTokenFactory.setToken(response.data.token);
-          $location.path('/activities');
-          }, 
-          function(err){
-            console.log(err);
-        })
-      }
+  $scope.signup = function() {
+    $http.post('/couples/signup', {
+        username: $scope.usernameSignup, 
+        password: $scope.passwordSignup,
+        firstName1: $scope.firstName1Signup,
+        lastName1: $scope.lastName1Signup,
+        firstName2: $scope.firstName2Signup,
+        lastName2: $scope.lastName2Signup,
+        email: $scope.emailSignup,
+        phoneNumber: $scope.phoneNumberSignup
+      })
+      .then(function(response) {
+        AuthTokenFactory.setToken(response.data.token);
+        $location.path('/activities');
+      }, 
+      function(err){
+        console.log(err);
+      });
+  };
 
-      $scope.signout = function(){
-        AuthTokenFactory.setToken();
-      }
+  $scope.signout = function() {
+    AuthTokenFactory.setToken();
+  };
 
-    })
+})
 
-.factory('AuthTokenFactory', function($window){
-      var key = 'JWT';
+.factory('AuthTokenFactory', function($window) {
+  var key = 'JWT';
 
-      return {
-        getToken: getToken,
-        setToken: setToken,
-        isAuth: isAuth
-      };
+  return {
+    getToken: getToken,
+    setToken: setToken,
+    isAuth: isAuth
+  };
 
-      function getToken(){
-        return $window.localStorage.getItem(key);
-      }
+  function getToken(){
+    return $window.localStorage.getItem(key);
+  }
 
-      function setToken(token){
-        if(token){
-          $window.localStorage.setItem(key, token)
-        } else {
-          $window.localStorage.removeItem(key);
-        }
-      }
+  function setToken(token){
+    if(token){
+      $window.localStorage.setItem(key, token)
+    } else {
+      $window.localStorage.removeItem(key);
+    }
+  }
 
-      function isAuth(){
-        return !! $window.localStorage.getItem(key);
-      }
-    });
+  function isAuth(){
+    return !! $window.localStorage.getItem(key);
+  }
+});
