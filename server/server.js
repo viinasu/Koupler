1a5e66547babff56bcd28876de9eff714bd0d819
Jackson Sierra <jks718@gmail.com>
2015-09-11 11:08:17 -0700

    [refactor] (server): update server to work on Digital Ocean

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+var http = require('http');
 var express = require('express');
 var middleware = require('./config/middleware.js');
+
 var app = express();
+var server = http.createServer(app);
 
 middleware(app, express);
-app.listen(3000);
-console.log("listening on 3000");
+
+server.listen(80, '104.236.172.91', function() {
+  console.log('Listening on 104.236.172.91:' + server.address().port);
+});
 
 module.exports = app;
