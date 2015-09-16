6706811e36be0d63b5300cdcbd5be17419d3ef85
darrinmn9 <dnagengast9@gmail.com>
2015-09-16 14:58:19 -0700

    added karma config, dependencies and modified file structure

---
+++ karma.config.js
@@ -0,0 +1,58 @@
+module.exports = function(config){
+
+  config.set({
+
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-mocks/angular-mocks.js',
+
+    //our app code
+    'client/app/**/*.js',
+
+    //our templates
+    'client/app/**/*.html',
+
+    //our spec files
+    'specs/client/**/*.js',
+    ],
+
+    exclude: [
+      'karma.conf.js'
+    ],
+
+    preprocessors: {
+      'client/app/**/*.html': ['ng-html12js'],
+      'client/app/**/*.js': ['coverage']
+    },
+
+    ngHtml2JsPreprocessor: {
+      stripPrefix: 'client/app/',
+      moduleName: 'templates'
+    },
+
+    reporters: ['progress', 'coverage', 'nyan', 'unicorn'],
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+    },
+
+    port: 8080,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['Chrome'],
+
+    singleRun: false
+
+
+  });
+};
