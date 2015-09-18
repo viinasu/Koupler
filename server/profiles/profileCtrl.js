d79dc53dc0c52d8fb01ec2d4971da037e5dde33c
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-18 01:14:48 -0700

    (feat) saves profile pic to assets/.... on server
    - assets/profile-pic/{username}/{filename}
    - does not save path location to db
    - does not change filename from original

--- server/profiles/profileCtrl.js
+++ server/profiles/profileCtrl.js
@@ -1,6 +1,9 @@
 var profile = require('./profileModel.js');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var formidable = require('formidable');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 var getUsername = function(token) {
   var decodedToken = jwt.decode(token);
@@ -21,5 +24,20 @@ module.exports = {
         res.end();
       }
     });
+  },
+
+  storeProfilePic: function(req, res, next) {
+    var form = new formidable.IncomingForm();
+    var targetPath = __dirname + "/../assets/profile-pic/" + req.params.username + "/";
+    mkdirp(targetPath, function(err) {
+      if (err) {throw err;}
+    });
+
+    form.parse(req, function(err, fields, files) {
+      fs.rename(files.file.path, targetPath + files.file.name);
+    });
+
+    res.send("thank you");
+    res.end();
   }
 };
