d0158bfb4f359d0a86c8942bfae1d8dcad797254
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-18 12:21:36 -0700

    (update) profile-pics now saved with defined names
    - example filename: darrin09-15-18.jpg
    - added formidable, mkdirp, moment to dependencies

--- server/profiles/profileModel.js
+++ server/profiles/profileModel.js
@@ -1,6 +1,8 @@
 var dbConnection = require('../db/index.js');
+var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
+var moment = require('moment');
 
 var queryDb = function(queryString, params, callback) {
   dbConnection.query(queryString, params, function(err, data) {
@@ -24,16 +26,23 @@ module.exports = {
   setProfilePic: function(params, callback) {
     var username = params[0];
     var file = params[1];
+    var fileExtension = path.extname(file.name);
+    var date = moment().format('MM-DD-YY');
 
-    var targetPath = __dirname + "/../assets/profile-pic/" + username + "/";
-    var filePathServer = targetPath + file.name;
+    // directory where image will be saved on the server
+    var targetPath = __dirname + "/../assets/" + username + "/profile-pic/";
 
+    // full path to image on server
+    var filePathServer = targetPath + username + date + fileExtension;
+
+    // make parent directories if doesn't exist
     mkdirp(targetPath, function(err) {
       if (err) {
         console.error(err);
       }
     });
 
+    // move and rename image from temp location to filePathServer
     fs.rename(file.path, filePathServer, function(err) {
       if (err) {
         console.error(err);
