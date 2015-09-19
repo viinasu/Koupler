da70b09f3c0a7a3c13dd0983614eccd104b3a278
mtcrushmore <ryanthesmith@gmail.com>
2015-09-18 17:17:40 -0700

    (mergefix) fixed small merge conflicts in schema.sql

--- server/activities/activityCtrl.js
+++ server/activities/activityCtrl.js
@@ -1,6 +1,10 @@
 var activity = require('./activityModel');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
+var q = require('q');
+var Promise = require('bluebird');
+var async = require('async');
+var request = Promise.promisifyAll(require('request'));
 
 var getUsername = function(token) {
   var decodedToken = jwt.decode(token);
@@ -38,9 +42,36 @@ module.exports = {
   getMatches: function(req, res, next) {
     var username = getUsername(req.body.token);
     var activityChosen = req.body.activity.name;
+    var userZipCode = req.body.userZipCode;
+    var searchDistance = req.body.searchDistance;
+    var responseData = [];
 
-    return activity.getMatches([ activityChosen ], function(err, data) {
-      res.send(data);
-    })
-  }
+    activity.getMatches([ activityChosen ], function(err, data) {
+      var allMatches = data;
+
+      var apiGetCount = 0;
+      var getDistance = function(match, collection) {
+
+        //query to Zip Code API to find distances between user's zip/city and matches' zip/city
+        //to match search parameters
+        var distanceQueryString = 'https://www.zipcodeapi.com/rest/jn6yHpsdgJEIQpLZPmNMX6Ik5xEuZbPjzVRglktB38jUx4UmrH9oNRJTpnoozdI8/distance.json/' + userZipCode + '/' + match.location_zip +'/miles'; 
+        request.get(distanceQueryString, function(err, data, body) {
+          body = JSON.parse(body);
+          //we must keep an API get counter, and only when all matches have been checked can we send a response
+          apiGetCount++;
+          if (body.distance <= searchDistance) {
+            collection.push(match);
+          }
+          //when all potential matches have been checked against the API's response, we send the response data array
+          if (apiGetCount === allMatches.length) {
+            res.send(collection);
+          }
+        })
+      }
+
+      for (var i = 0; i < allMatches.length; i++) {
+        getDistance(allMatches[i], responseData);
+      };
+    });   
+  } 
 };
