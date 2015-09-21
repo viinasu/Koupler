2c06b9901c7ff096d4cb2a2b48a05af95ba19624
ccheung0926 <cindycheung0926@gmail.com>
2015-09-20 22:59:28 -0700

    (updated) updated chat init and send message function

--- client/app/profile/chat.js
+++ client/app/profile/chat.js
@@ -1,20 +1,64 @@
 angular.module('koupler.chat', [])
 
-.controller('ChatCtrl', ['$scope','socket',function($scope, socket){
+.controller('ChatCtrl', ['$scope','$http','socket',function($scope, $http, socket){
   var vm = this;
   vm.openChatBox = false;
+  vm.sender = $scope.$parent.sender;
+  vm.chatStart = true;
+
+  vm.closeBox = function() {
+    vm.chatStart = false;
+  }
+  console.log('chatStart', vm.chatStart);
+
+  vm.tempMsgStorage = [
+    {name: 'Cindy', msg: 'Hello'},
+    {name: 'Ting', msg: 'Whats up'},
+    {name: 'Cindy', msg: 'How you doing?'},
+    {name: 'Ting', msg: 'fsdfdsfkdsfksd jkfjslkfjsklfjdslfjdslkfjdsklfjsdsdkfldflsdjfkljsdfsdkfjslkfjlksdjfklsd'},
+    {name: 'Cindy', msg: "Here is a paragraph with a lot of text, but only two visible lines. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum purus vitae est accumsan cursus. Donec molestie pretium nulla at blandit. Quisque eget augue et nibh dapibus vulputate. Donec nec fermentum mauris. Vivamus non ipsum sed felis viverra molestie id ac neque. Sed purus orci, egestas pellentesque dapibus id, tempor id turpis. Morbi facilisis massa purus. Donec a urna facilisis odio varius hendrerit. Nulla diam ligula, ultrices ac volutpat nec, molestie sit amet est. Pellentesque imperdiet interdum laoreet. Phasellus tincidunt felis eu urna accumsan luctus. Sed nec nisi leo. Pellentesque luctus porta turpis non sagittis. Phasellus sodales sem nec urna viverra eget porta justo tincidunt. Maecenas iaculis laoreet turpis, eu malesuada est lacinia id. Nunc arcu tellus, ultricies at vestibulum sit amet, malesuada non purus."}
+  ];
 
   vm.openClose = function() {
-    if(vm.openChatBox) {
+    console.log('chat sender', vm.sender);
+    if(!vm.openChatBox) {
       vm.openChatBox = true;
     }
     else {
       vm.openChatBox = false;
     }
+    console.log('$scope.parent', $scope.$parent.sender);
   };
-  vm.closeConversation = function(){
-    $scope.$parent.openConversation = false;
-    console.log($scope.$parent.openConversation);
-  };
+
+  vm.sendMessage = function(){
+    var msg =  {
+      to: vm.receiverUsername,
+      from: vm.sender,
+      message: vm.message
+    };
+    socket.emit('sendMessageToServer', msg);
+
+    $http.post('/chat', msg)
+      .then(function(response){
+        console.log('response',response);
+      }),
+      function(err){
+        console.log(err);
+      };
+    vm.message = "";
+  }
+
+  //get receiver's username from socket
+  socket.on('getNamefromServer', function(name) {
+  //{receiverUsername: "beckhams",
+  // couples1: "Victoria Beckham", couples2: "David Beckham"}
+    vm.name = name.couples1 + " & " + name.couples2;
+    vm.receiverUsername = name.receiverUsername;
+  });
+
+  //user received message
+  socket.on('receivedMessage', function(data) {
+
+  });
 
 }]);
--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -2,16 +2,16 @@ angular.module('koupler.profile', [
   'ui.bootstrap'
   ])
 
-.controller('ProfileCtrl', ['$scope', '$state', '$modal', '$http', 'Activities', 'AuthTokenFactory', 'Upload', function($scope, $state, $modal, $http, Activities, AuthTokenFactory, Upload) {
+.controller('ProfileCtrl', function($scope, $state, $http, Activities, AuthTokenFactory, Upload, socket) {
 
   var vm = this;
   //placeholder for POST request until routeParam is set up
   vm.username = $state.params.username;
+  $scope.sender = $scope.$parent.sender;
 
   vm.goToActivities = function() {
     $state.go('activities');
   };
-
   vm.profileData = {};
 
   vm.getProfileInfo = function() {
@@ -27,8 +27,8 @@ angular.module('koupler.profile', [
         vm.profileData = response.data[0];
       });
   };
-
   vm.getProfileInfo();
+
 
   vm.showEditModal = function() {
     var editModal = $modal.open({
@@ -57,13 +57,30 @@ angular.module('koupler.profile', [
     }
   };
 
-  vm.chatInit = function(receiver) {
-    if(!$scope.openConversation) {
-      $scope.openConversation = true;
-    }
-    console.log($scope.openConversation);
+  socket.on('senderNameToClient', function(sender){
+    vm.sender = sender;
+  });
 
-  };
-
-}]);
-
+  vm.chatInit = function() {
+    $scope.openConversation = true;
+    //get sender's username
+    //ctrl.profileData.username
+    // profileData.person_1_first_name
+    //                    2 last name
+    
+    //get chat history
+    $http.get('/chat')
+      .then(function(response) {
+        if (response.data[0].isAuthorizedToEdit) {
+          vm.isAuthorizedToEdit = true;
+        };
+        $scope.chatData = response.data;
+        console.log('chat hist.', $scope.chatData);
+      });
+    socket.emit('sendReceiverToServer', {
+      receiverUsername: vm.profileData.username,
+      couples1: vm.profileData.person_1_first_name + " " + vm.profileData.person_1_last_name,
+      couples2: vm.profileData.person_2_first_name + " " + vm.profileData.person_2_last_name
+    });
+  }
+})
